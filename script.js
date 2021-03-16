72be0bf7b4cd166cb81d61a6194753f7e88d8c6a
¨Marco¨ <m.peters646@gmail.com>
2021-03-16 21:06:20 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const buttons = document.querySelectorAll('.big-five-button');
+const buttons = document.querySelectorAll(".big-five-button");
 
-buttons.forEach((button) => {
-  button.addEventListener('click', () => console.log(`Clicked on ${button.innerText}!`));
+const newAnimal = buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    // console.log(button.innerText);
+    return button.innerText;
+  });
 });
 
+const newLi = document.createElement("li");
+newLi.className = "big-five-list-item";
+
+const newSpottedAnimal = document.getElementById("spotted-animals-list");
+newSpottedAnimal.appendChild(newLi);
+// newLi.appendChild(newAnimal);
+
 // console.log(buttons);
+// console.log(newLi);
+console.log(newAnimal);
+console.log(newSpottedAnimal);

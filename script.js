7ce0174d26c2a2762ca1ee44ce0f9563a1a7f0c7
Marco Peters <m.peters646@gmail.com>
2021-03-16 22:14:03 +0100

    Added one new animal

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-const buttons = document.querySelectorAll(".big-five-button");
+const buttons = document.querySelectorAll('.big-five-button');
+const newSpottedAnimal = document.getElementById('spotted-animals-list');
+let newLi = document.createElement('li');
 
-const newAnimal = buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    // console.log(button.innerText);
-    return button.innerText;
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    newLi.className = 'spotted-animals-list-item';
+    newSpottedAnimal.appendChild(newLi);
+    newLi.innerText = button.innerHTML;
   });
 });
-
-const newLi = document.createElement("li");
-newLi.className = "big-five-list-item";
-
-const newSpottedAnimal = document.getElementById("spotted-animals-list");
-newSpottedAnimal.appendChild(newLi);
-// newLi.appendChild(newAnimal);
-
-// console.log(buttons);
-// console.log(newLi);
-console.log(newAnimal);
-console.log(newSpottedAnimal);

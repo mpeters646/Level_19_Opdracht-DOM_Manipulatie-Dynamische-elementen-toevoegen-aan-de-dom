514982ee9cde79e6e2b9dfc09f78988dbc9ab3d7
Marco Peters <m.peters646@gmail.com>
2021-03-17 11:43:48 +0100

    Updated some css styles & completed the remove buttons

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const buttons = document.querySelectorAll('.big-five-button');
-const newSpottedAnimal = document.getElementById('spotted-animals-list');
-let newLi = document.createElement('li');
+const spottedAnimalButtons = document.querySelectorAll('.big-five-button');
+const removeFirstAnimalButton = document.getElementById('remove-first-item-button');
+const removeAllAnimalsButton = document.getElementById('remove-all-button');
 
-buttons.forEach((button) => {
+spottedAnimalButtons.forEach((button) => {
   button.addEventListener('click', () => {
+    const newSpottedAnimal = document.getElementById('spotted-animals-list');
+    const newLi = document.createElement('li');
     newLi.className = 'spotted-animals-list-item';
     newSpottedAnimal.appendChild(newLi);
     newLi.innerText = button.innerHTML;
   });
+});
+
+removeFirstAnimalButton.addEventListener('click', () => {
+  let list = document.getElementById('spotted-animals-list');
+  if (list.hasChildNodes()) {
+    list.removeChild(list.childNodes[0]);
+  }
+});
+
+removeAllAnimalsButton.addEventListener('click', () => {
+  let list = document.getElementById('spotted-animals-list');
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
 });
